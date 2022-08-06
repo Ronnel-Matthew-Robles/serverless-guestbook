3c1552c91d0a6bf160df12e86fc9d3e3a625de78
Matt <53041636+Ronnel-Matthew-Robles@users.noreply.github.com>
2022-08-06 12:02:05 +0800

    added API url in guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://b7f6465d.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
